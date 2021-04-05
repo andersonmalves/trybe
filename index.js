e627393b364162a5327b9a886152b3afea4c9883
Anderson Meira <contatodeanderson@gmail.com>
2021-04-05 18:46:22 -0300

    endpoint GET /crush/:id

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
+const NOT_FOUND = 404;
 const PORT = '3000';
 
 async function getCrush() {
@@ -13,9 +14,18 @@ async function getCrush() {
   return JSON.parse(data);
 }
 
-app.get('/crush', async (req, response) => {
+app.get('/crush', async (_request, response) => {
   const data = await getCrush();
   response.status(SUCCESS).send(data);
+});
+
+app.get('/crush/:id', async (_request, response) => {
+  const data = await getCrush();
+  const result = data.find((el) => el.id === parseInt(_request.params.id, 10));
+  if (result === undefined) { 
+    return response.status(NOT_FOUND).send({ message: 'Crush não encontrado' });
+  }
+  response.status(SUCCESS).send(result);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
