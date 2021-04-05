e72308ec037831a25d32ef9932feadcf86e4da5d
Anderson Meira <contatodeanderson@gmail.com>
2021-04-05 17:49:23 -0300

    endpoint GET /crush

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
 const PORT = '3000';
+
+async function getCrush() {
+  const data = await fs.readFile('./crush.json');
+  return JSON.parse(data);
+}
+
+app.get('/crush', async (req, response) => {
+  const data = await getCrush();
+  response.status(SUCCESS).send(data);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
