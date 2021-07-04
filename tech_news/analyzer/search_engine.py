e66ad7cc9bfce3c51fd36ae6d63f4d3acb734a9a
Anderson Meira <contatodeanderson@gmail.com>
2021-07-04 19:57:07 -0300

    add: requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import find_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    all_news = find_news()
+    result = []
+
+    for news in all_news:
+        if title.lower() in news['title'].lower():
+            result.append((news['title'], news['url']))
+    return result
 
 
 # Requisito 7
@@ -16,3 +25,6 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+# search_by_title("Musk")
