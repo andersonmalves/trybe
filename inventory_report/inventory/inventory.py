4c7b1c5e94d3f5bcd028bb29063c7f465fd0bdad
renatomak <renato.mark.silva@gmail.com>
2021-07-22 15:57:19 -0300

    RQ3 - finalizado

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -3,6 +3,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xml.etree.ElementTree as ET
 
 
 class Importer(ABC):
@@ -53,32 +54,26 @@ class TransformDataJSON(Importer):
 
 class TransformDataXML(Importer):
     def de_serialize(self):
-        with open(self.filepath) as file:
-            print(file.read())
-            return file.read()
+        tree = ET.parse(self.filepath)
+        root = tree.getroot()
+        return root
 
     def transform_data(self, file_reader):
         list_file = []
-        return list_file
-
-
-class Inventory:
-    def transform_data(file):
-        file.__next__()
-        list_file = []
-
-        for row in file:
+        for child in file_reader:
             obj = {
-                "id": int(row[0]),
-                "nome_do_produto": row[1],
-                "nome_da_empresa": row[2],
-                "data_de_fabricacao": row[3],
-                "data_de_validade": row[4],
-                "numero_de_serie": row[5],
+                "id": int(child[0].text),
+                "nome_do_produto": child[1].text,
+                "nome_da_empresa": child[2].text,
+                "data_de_fabricacao": child[3].text,
+                "data_de_validade": child[4].text,
+                "numero_de_serie": child[5].text,
             }
             list_file.append(obj)
         return list_file
 
+
+class Inventory:
     @classmethod
     def import_data(cls, export_file, type_report="simples"):
         dictionary = ""
@@ -93,43 +88,9 @@ class Inventory:
 
         elif ".xml" in export_file:
             content_xml = TransformDataXML(export_file)
-            print(content_xml)
-            dictionary = "XML"
+            xml_report = content_xml.de_serialize()
+            dictionary = content_xml.transform_data(xml_report)
 
         if type_report == "completo":
             return CompleteReport.generate(dictionary)
         return SimpleReport.generate(dictionary)
-
-
-# Para testar
-# relatorio = Inventory.import_data(
-#     "inventory_report/data/inventory.csv", "simples"
-# )
-
-# print(relatorio)
-
-# relatorio_completo = Inventory.import_data(
-#     "inventory_report/data/inventory.csv", "completo"
-# )
-# print(relatorio_completo)
-
-
-# relatorio_completo = Inventory.import_data(
-#     "inventory_report/data/inventory.json", "completo"
-# )
-# relatorio = Inventory.import_data(
-#     "inventory_report/data/inventory.json", "simples"
-# )
-
-# print(relatorio)
-# print(relatorio_completo)
-
-relatorio_completo = Inventory.import_data(
-    "inventory_report/data/inventory.xml", "completo"
-)
-relatorio = Inventory.import_data(
-    "inventory_report/data/inventory.xml", "simples"
-)
-
-print(relatorio)
-print(relatorio_completo)
