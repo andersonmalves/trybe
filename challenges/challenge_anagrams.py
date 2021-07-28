3efa92193a252e63964187e75a273c791ecfc2fb
Anderson Meira <contatodeanderson@gmail.com>
2021-07-27 23:30:00 -0300

    add: Anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
+from typing import Counter
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return Counter(first_string) == Counter(second_string)
+
+
+# https://stackoverflow.com/questions/48217471/is-it-possible-to-check-for-anagram-without-using-sorted-or-dictionary-that-pe
+first_string = "pedra"
+second_string = "perda"
+print(is_anagram(first_string, second_string))
