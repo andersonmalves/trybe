41880bc99a6c8793cadc35989595d046b0b6343c
Anderson Meira <contatodeanderson@gmail.com>
2021-08-15 17:58:31 -0300

    Requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,67 @@
+import collections
+
+
 class TrackOrders:
     def __len__(self):
-        pass
+        return len(self.pedidos)
+
+    def __init__(self):
+        self.pedidos = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.pedidos.append({"a": costumer, "b": order, "c": day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        lista_do_consumidor = []
+        for row in self.pedidos:
+            if row["a"] == costumer:
+                lista_do_consumidor.append(row["b"])
+        return collections.Counter(lista_do_consumidor).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        c = set()
+        d = set()
+
+        for row in self.pedidos:
+            if row["a"] == costumer:
+                d.add(row["b"])
+            c.add(row["b"])
+
+        return c.difference(d)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        e = set()
+        f = set()
+
+        for row in self.pedidos:
+            if row["a"] == costumer:
+                f.add(row["c"])
+            e.add(row["c"])
+
+        return e.difference(f)
 
     def get_busiest_day(self):
-        pass
+        g = []
+
+        for row in self.pedidos:
+            g.append(row["c"])
+        
+        return collections.Counter(g).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        h = []
+
+        for row in self.pedidos:
+            h.append(row["c"])
+        
+        return collections.Counter(h).most_common()[-1][0]
+
+
+
+track_orders = TrackOrders()
+track_orders.add_new_order("jorge", "frango", "domingo")
+track_orders.add_new_order("maria", "hamburguer", "terca-feira")
+track_orders.add_new_order("arnaldo", "misto-quente", "terca-feira")
+
+most_ordered = track_orders.get_most_ordered_dish_per_costumer("maria")
+print(most_ordered)
