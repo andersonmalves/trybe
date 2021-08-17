75210e1e1c5f29f918bfd5d07fc524801be760a7
Anderson Meira <contatodeanderson@gmail.com>
2021-08-17 16:08:34 -0300

    Requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,37 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.fila = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.fila)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.fila.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.fila:
+            return self.fila.pop(0)
+        return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self.fila):
+            return self.fila[index]
+        else:
+            raise IndexError
+
+    def __str__(self):
+        return "Deque(" + ", ".join(map(lambda x: str(x), self.fila)) + ")"
+
+
+if __name__ == "__main__":
+    queue = Queue()
+    elements_1 = [6, 7, 8, 9, 10]
+    elements_2 = [1, 2, 3, 4, 5]
+
+    # for elem in elements_1:
+    #     deque.push_front(elem)
+
+    for elem in elements_2:
+        queue.enqueue(elem)
+
+    print(queue)
